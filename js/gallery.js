28ccdb6f131e36bc1a0e615c6b38788128849fb4
Mark Collis <mark@markandblanka.com>
2018-09-18 15:37:54 +0200

    Implemented image selection from thumbnails

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,29 @@
 function activateGallery() {
-	alert("Hello from the gallery file!");
+	// alert("Hello from the gallery file!");
+
+let thumbnails = document.querySelectorAll("#gallery-thumbs img");
+let thumbnailDivs = document.querySelectorAll("#gallery-thumbs div");
+let mainImage = document.querySelector("#gallery-photo > img");
+let imageTitle = document.querySelector("#gallery-info > h3");
+let imageDescription = document.querySelector("#gallery-info > p");
+
+
+thumbnails.forEach(function(thumbnail) {
+	thumbnail.addEventListener("click", function() {
+		// code to set clicked image as main image
+		mainImage.setAttribute("src", thumbnail.dataset.largeVersion);
+		mainImage.setAttribute("alt", thumbnail.alt);
+
+		// console.log(imageTitle);
+		imageTitle.innerHTML = thumbnail.dataset.title;
+		// console.log(imageDescription);
+		imageDescription.innerHTML = thumbnail.dataset.description;
+
+		thumbnailDivs.forEach(function(thumbnailDiv) {
+			thumbnailDiv.setAttribute("class", "");
+		})
+		thumbnail.parentElement.setAttribute("class", "current");
+
+	});
+});
 }
