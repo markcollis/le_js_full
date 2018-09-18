ccbab2a53fce75bcfd4221358a2aaa5f86e51f59
Mark Collis <mark@markandblanka.com>
2018-09-18 16:01:42 +0200

    Preload large images

--- js/gallery.js
+++ js/gallery.js
@@ -9,16 +9,23 @@ let imageDescription = document.querySelector("#gallery-info > p");
 
 
 thumbnails.forEach(function(thumbnail) {
+	// code to preload large images
+	let newImageSrc = thumbnail.dataset.largeVersion;
+	let largeVersion = new Image();
+	largeVersion.src = newImageSrc;
+
 	thumbnail.addEventListener("click", function() {
 		// code to set clicked image as main image
-		mainImage.setAttribute("src", thumbnail.dataset.largeVersion);
+		mainImage.setAttribute("src", newImageSrc);
 		mainImage.setAttribute("alt", thumbnail.alt);
 
+		// code to match associated text to image
 		// console.log(imageTitle);
 		imageTitle.innerHTML = thumbnail.dataset.title;
 		// console.log(imageDescription);
 		imageDescription.innerHTML = thumbnail.dataset.description;
 
+		// code to highlight thumbnail for current image
 		thumbnailDivs.forEach(function(thumbnailDiv) {
 			thumbnailDiv.setAttribute("class", "");
 		})
